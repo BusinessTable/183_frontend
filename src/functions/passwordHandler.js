f4c82caf2e623ac0f77807d55ad5f73e705d3b26
Ares Dovalo Taboas <ares.dovalo@netcetera.com>
2024-05-30 14:58:27 +0200

    added register

--- src/functions/passwordHandler.js
+++ src/functions/passwordHandler.js
@@ -3,14 +3,15 @@ import axios from "axios";
 export async function register(username, password) {
   let config = {
     method: "post",
+    maxBodyLength: Infinity,
     url: "https://139.162.128.79:5005/register",
     headers: {
       "Content-Type": "application/json",
     },
-    data: JSON.stringify({username:username, masterPassword:password }),
+    data: JSON.stringify({ username: username, masterPassword: password }),
   };
 
-  await axios
+  return await axios
     .request(config)
     .then((response) => {
       return JSON.stringify(response.data);
