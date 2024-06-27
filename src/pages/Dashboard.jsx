34848bf838c1514c56e6602eea37bf3d0bc25fcc
Ares Dovalo Taboas <ares.dovalo@netcetera.com>
2024-06-27 16:36:49 +0200

    added pagination

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,41 +1,47 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import LogoutIcon from '@mui/icons-material/Logout';
-import Table from '../components/Table';
-import AddPassword from '../components/AddPassword'; // Import the new AddPassword component
-import useAuth from '../hooks/useAuth';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Pagination from "@mui/material/Pagination";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { getPasswords } from "../functions/passwordHandler";
+import AddPassword from "../components/AddPassword";
+import useAuth from "../hooks/useAuth";
 
 const drawerWidth = 240;
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  '& .MuiDrawer-paper': {
-    position: 'relative',
-    whiteSpace: 'nowrap',
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    boxSizing: 'border-box',
+    boxSizing: "border-box",
     ...(!open && {
-      overflowX: 'hidden',
-      transition: theme.transitions.create('width', {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
       }),
       width: theme.spacing(7),
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: theme.spacing(9),
       },
     }),
@@ -43,21 +49,36 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 export default function Dashboard() {
-  const { logout } = useAuth();
+  const { logout, authed } = useAuth();
+  const [rows, setRows] = React.useState([]);
+  const [pageNumbers, setPageNumbers] = React.useState(1);
+  const [page, setPage] = React.useState(1);
   const [open, setOpen] = React.useState(true);
+
+  const handleChange = (event, value) => {
+    setPage(value)
+  };
+
+  React.useEffect(() => {
+    getPasswords(authed, page).then((x) => {
+      setRows(x.passwords);
+      setPageNumbers(x.totalPages);
+    });
+  }, [authed, page]);
+
   const toggleDrawer = () => {
     setOpen(!open);
   };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <Drawer variant="permanent" open={open}>
         <Toolbar
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between', // Adjust to space-between
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between", // Adjust to space-between
             px: [1],
           }}
         >
@@ -73,26 +94,44 @@ export default function Dashboard() {
         component="main"
         sx={{
           backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[100]
-              : theme.palette.grey[900],
+            theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[900],
           flexGrow: 1,
-          height: '100vh',
-          overflow: 'auto',
+          height: "100vh",
+          overflow: "auto",
         }}
       >
         <Toolbar />
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-          <Grid container spacing={3}>
+          <Grid container spacing={3} style={{ display: "flex", flexDirection: "row-reverse" }}>
             {/* Recent Orders */}
             <Grid item xs={12}>
-              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                <Table />
+              <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                <Table size="small">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>username</TableCell>
+                      <TableCell>pwd</TableCell>
+                      <TableCell>url</TableCell>
+                      <TableCell>notes</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row, idx) => (
+                      <TableRow key={idx}>
+                        <TableCell>{row.username}</TableCell>
+                        <TableCell>{row.pwd}</TableCell>
+                        <TableCell>{row.url}</TableCell>
+                        <TableCell>{row.notes}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
               </Paper>
             </Grid>
+            <Pagination count={pageNumbers} page={page} size="small" onChange={handleChange} />
             {/* Add Password */}
             <Grid item xs={12}>
-              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
                 <AddPassword />
               </Paper>
             </Grid>
