34319967ce714765ffdaf2c748409a813cab7775
Ares Dovalo Taboas <ares.dovalo@netcetera.com>
2024-07-11 15:58:55 +0200

    dropdown fixes

--- src/components/RubricDropdown.jsx
+++ src/components/RubricDropdown.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Autocomplete,
   TextField,
@@ -7,25 +7,28 @@ import {
   ListItemText,
   ListItem,
   Paper,
-} from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import AddButtons from './AddButtons';
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import AddButtons from "./AddButtons";
 
-export default function RubricDropdown({
-  rubrics,
-  handleEditRubric,
-  handleAddRubric,
-}) {
+export default function RubricDropdown({ rubrics, handleEditRubric, handleAddRubric, handleRubricSelection }) {
+  const onChange = (event, newValue) => {
+    handleRubricSelection(newValue);
+  };
+
+  const isOptionEqualToValue = (option, value) => option.uuid === value.uuid;
+
   return (
-    <Paper sx={{ display: 'flex' }}>
+    <Paper sx={{ display: "flex" }}>
       <AddButtons onClick={handleAddRubric} />
       <Autocomplete
         sx={{ flex: 1 }}
         options={rubrics}
         getOptionLabel={(rubric) => rubric.name}
+        isOptionEqualToValue={isOptionEqualToValue}
         renderInput={(params) => <TextField {...params} label="Rubrics" />}
         renderOption={(props, rubric) => (
-          <ListItem {...props} key={rubric.uuid}>
+          <ListItem {...props} key={rubric.uuid} component="div">
             <ListItemText primary={rubric.name} />
             <ListItemSecondaryAction>
               <IconButton onClick={() => handleEditRubric(rubric)}>
@@ -34,6 +37,7 @@ export default function RubricDropdown({
             </ListItemSecondaryAction>
           </ListItem>
         )}
+        onChange={onChange}
       />
     </Paper>
   );
